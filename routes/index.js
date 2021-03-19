5d62d5eb9934236e1cda3a475a93ef9bab64bc68
j-lindsey <j-lindsey@users.noreply.github.com>
2021-03-19 16:31:29 -0600

    adding database connection

--- routes/index.js
+++ routes/index.js
@@ -20,10 +20,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'test',
-    database: 'Simfoni'
+    host: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
